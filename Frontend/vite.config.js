a336b3955f051f8054e106214f418d6b76c0aad8
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-03-05 01:33:50 +0530

    fix : daisyui problem fix by installing as dependencies not in dev

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
   build: {
     outDir: 'dist',  // This is default, but confirm it
